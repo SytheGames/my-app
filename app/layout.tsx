5357350ff3321e14d5d6b189adf48c4bbf37d83c
Matt Kealey <matt@kealeydesign.ca>
2026-03-17 16:41:51 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,6 +96,19 @@ export default function RootLayout({
         {children}
         <SpeedInsights/>
       </body>
+        <Script id="apollo-tracker" strategy="afterInteractive">
+          {`
+            function initApollo(){
+              var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+              o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n;
+              o.async=true;
+              o.defer=true;
+              o.onload=function(){window.trackingFunctions.onLoad({appId:"69b9b72bab33d10019f776e2"})};
+              document.head.appendChild(o);
+            }
+            initApollo();
+          `}
+        </Script>
       {hasGa ? (
         <>
           <Script
